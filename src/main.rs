aaace6b6892539c40239a7bd5ad101bf44bbea00
Andrew [RTech] <in0mad91@gmail.com>
2021-02-15 15:40:41 +0200

    Configure checking of dia file existence

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,19 @@
 #[macro_use]
 extern crate clap;
 use clap::App;
+use std::path::Path;
 
 fn main() {
     let yaml = load_yaml!("cli.yml");
     let matches = App::from_yaml(yaml).get_matches();
-    println!("{:?}", matches);
+    let keys = matches.args.keys();
+    for key in keys {
+        if key == &"from-dia" || key == &"f" {
+            let path = Path::new(matches.value_of(key).unwrap());
+            if !path.exists() || !path.is_file() {
+                println!("Path {:?} not exists!", path);
+            }
+        }
+        continue;
+    }
 }
